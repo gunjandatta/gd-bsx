54a104e2d3f3a11beab976b5801bd066cfa55142
Gunjan Datta <me@dattabase.com>
2020-05-27 16:44:52 -0400

    Updated the exports.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-// Export the JS components
-import Components from "./lib";
-export { Components }
-
 // Export the react components
-import * as ReactComponents from "./components";
 export * from "./components";
+import Components from "./lib";
 
-// Update the global variable
-window["GD"] = { ...window["GD"], ...ReactComponents };
-
-export default ReactComponents;
+// Export the component types
+export const AlertTypes = Components.AlertTypes;
+export const BadgeTypes = Components.BadgeTypes;
+export const CheckboxGroupTypes = Components.CheckboxGroupTypes;
+export const DropdownTypes = Components.DropdownTypes;
+export const FormControlTypes = Components.FormControlTypes;
+export const InputGroupTypes = Components.InputGroupTypes;
+export const ListGroupItemTypes = Components.ListGroupItemTypes;
+export const MediaImageTypes = Components.MediaImageTypes;
+export const NavbarTypes = Components.NavbarTypes;
+export const PanelTypes = Components.PanelTypes;
+export const PopoverTypes = Components.PopoverTypes;
+export const SpinnerTypes = Components.SpinnerTypes;
+export const TooltipType = Components.TooltipTypes;
--- src/lib.ts
+++ src/lib.ts
@@ -3,6 +3,7 @@ import { Components } from "gd-bs";
 // Ensure the Components reference exists
 if (Components == null) {
     // Update it to the global variable
+    console.log("[Dev] The components variable is null. Defaulting to the global variable.");
     (Components as any) = window["GD"] && window["GD"].Components;
 }
 
