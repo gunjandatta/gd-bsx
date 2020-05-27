dbfd1fd670f2958f9b23a6e229a6b80c8622c526
Gunjan Datta <me@dattabase.com>
2020-05-27 17:14:05 -0400

    Updated the exports.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // Export the react components
 export * from "./components";
 
+// Export the components
+import Components from "./lib";
+export { Components }
+
 // Update the global variable
-import * as Components from "./components";
-window["GD"] = { ...window["GD"], ...Components };
+import * as ReactComponents from "./components";
+window["GD"] = { ...window["GD"], ...ReactComponents };
