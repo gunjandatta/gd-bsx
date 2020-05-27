27b931011b1169c1dd3ca7bee588c7dc68498add
Gunjan Datta <me@dattabase.com>
2020-05-27 16:56:44 -0400

    Remoted the types from the exports.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
 // Export the react components
 export * from "./components";
-import Components from "./lib";
 
-// Export the component types
-export const AlertTypes = Components.AlertTypes;
-export const BadgeTypes = Components.BadgeTypes;
-export const CheckboxGroupTypes = Components.CheckboxGroupTypes;
-export const DropdownTypes = Components.DropdownTypes;
-export const FormControlTypes = Components.FormControlTypes;
-export const InputGroupTypes = Components.InputGroupTypes;
-export const ListGroupItemTypes = Components.ListGroupItemTypes;
-export const MediaImageTypes = Components.MediaImageTypes;
-export const NavbarTypes = Components.NavbarTypes;
-export const PanelTypes = Components.PanelTypes;
-export const PopoverTypes = Components.PopoverTypes;
-export const SpinnerTypes = Components.SpinnerTypes;
-export const TooltipType = Components.TooltipTypes;
+// Update the global variable
+import * as Components from "./components";
+window["GD"] = { ...window["GD"], ...Components };
